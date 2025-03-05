7cbad1d1f1f742788d9d2ff704ddfc99668db01f
ssd <2537916417@qq.com>
2025-03-05 21:59:41 +0800

    refactor(src): 题目三

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <!-- 主内容区域，根据当前激活的标签显示不同组件 -->
-    <div class="content-area">
-      <basic v-if="currentTab === 'basic'"></basic>
-      <charts v-if="currentTab === 'charts'"></charts>
-    </div>
-    
-    <!-- 底部导航栏 -->
-    <tab-bar @tab-change="handleTabChange"></tab-bar>
+    <charts></charts>
   </div>
 </template>
 
 <script>
-// import DataToChart from './components/Charts.vue'
+import charts from './components/Charts.vue'
 //  import DataToChart from './components/Basic.vue'
 
 // import DataToChart from './components/dataToChart/dataToChart copy 2.vue'
 // import DataToChart from './components/chartComp/DashBoardChartA.vue'
-import tabBar from './components/tabBar/tabBar.vue'
-import basic from './components/Basic.vue'
-import charts from './components/Charts.vue'
+// import tabBar from './components/tabBar/tabBar.vue'
+// import basic from './components/Basic.vue'
+// import charts from './components/Charts.vue'
 
 
 export default {
   name: 'App',
   components: {
-    tabBar,
-    basic,
+
     charts  
   },
   data() {
